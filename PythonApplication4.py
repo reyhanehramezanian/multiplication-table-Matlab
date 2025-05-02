b0f0ada7914930a89ceb3760edf302ecea66fadd
Reyhaneh Ramazanian Moghaddam <reyhaneh.ram83@gmail.com>
2025-05-02 18:17:36 +0330

    Update Matlab

--- PythonApplication4.py
+++ PythonApplication4.py
@@ -1,6 +1,19 @@
+In general, the multiplication table in MATLAB looks like this:
+n=input('entr the value:')
+for i=1:n
+    for j=1:n
+        t(i,j)=i*j;
+    end
+end
+disp(t);
+
+For example, for the ten-by-ten multiplication table we have:
+n=10
 for i=1:10
     for j=1:10
-    MT(i,j)=i*j;
-   end
+       t(i,j)=i*j;
+    end
 end
-disp(MT)
+disp(t);
+
+
